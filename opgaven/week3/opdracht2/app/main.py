2261f1a83a62efe461d48eaa6ae1070eb95cb8be
lassebenni <lasse_benninga@hotmail.com>
2015-10-08 16:58:21 +0200

    opgave 2 week3

--- opgaven/week3/opdracht2/app/main.py
+++ opgaven/week3/opdracht2/app/main.py
@@ -23,11 +23,6 @@ def main():
     return render_template('index.html')
 
 
-@app.route("/showLogin")
-def showLogin():
-    return render_template('inloggen.html')
-
-
 @app.route("/showSignUp")
 def showSignUp():
     return render_template('aanmelden.html')
@@ -41,78 +36,8 @@ def userHome():
         return render_template('error.html', error='Unauthorized Access')
 
 
-@app.route('/logout')
-def logout():
-    session.pop('user', None)
-    return redirect('/')
 
 
-@app.route('/signUp', methods=['POST'])
-def signUp():
-    try:
-        # read the posted values from the UI
-        _name = request.form['inputName']
-        _email = request.form['inputEmail']
-        _password = request.form['inputPassword']
-        _phoneNumber = request.form['inputPhonenumber']
-
-        # validate the received values
-        if _name and _email and _password and _phoneNumber:
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            # _hashed_password = generate_password_hash(_password)
-            cursor.callproc('sp_createUser',
-                            (_name, _email, _password, _phoneNumber))
-            data = cursor.fetchall()
-
-            if len(data) is 0:
-                conn.commit()
-                cursor.callproc('sp_validateLogin', (_email,))
-                data = cursor.fetchall()
-                if _password == str(data[0][3]):
-                    session['user'] = data[0][0]
-
-                return redirect('/userHome')
-            else:
-                return json.dumps({'error': str(data[0])})
-        else:
-            return json.dumps({'html': '<span>Enter the required fields</span>'})
-
-    except Exception as e:
-        return json.dumps({'error': str(e)})
-    finally:
-        cursor.close()
-        conn.close()
-
-
-@app.route('/validateLogin', methods=['POST'])
-def validateLogin():
-    try:
-        _email = request.form['inputEmail']
-        _password = request.form['inputPassword']
-
-        # connect to mysql
-
-        con = mysql.connect()
-        cursor = con.cursor()
-        cursor.callproc('sp_validateLogin', (_email,))
-        data = cursor.fetchall()
-
-        if len(data) > 0:
-            if _password == str(data[0][3]):
-                session['user'] = data[0][0]
-                return redirect('/userHome')
-            else:
-                return render_template('error.html', error='Wrong Email address or Password.')
-        else:
-            return render_template('error.html', error='Wrong Email address or Password.')
-
-
-    except Exception as e:
-        return render_template('error.html', error=str(e))
-    finally:
-        cursor.close()
-        con.close()
 
 
 @app.route('/showHouseholds')
@@ -225,17 +150,14 @@ def addDevice():
         _name = request.form['inputName']
         _brand = request.form['inputBrand']
         _type = request.form['inputType']
-        _user = '66'
+        _huishouden = '64'
 
         if _name and _brand and _type:
 
             conn = mysql.connect()
             cursor = conn.cursor()
-            addQuery = 'START TRANSACTION; INSERT INTO apparaat(naam, merk, apparaat_type_fk)' \
-                       ' VALUES (%s,%s,%s); INSERT INTO apparaat_huishouden(huishouden_fk, apparaat_fk)' \
-                       'VALUES (%s, (SELECT LAST_INSERTED_ID())) COMMIT;'
 
-            cursor.execute(addQuery, (_name, _brand, _type, _user))
+            cursor.callproc('sp_addDevice', (_name, _brand, _type, _huishouden))
             result = cursor.fetchall()
             if len(result) is 0:
                 conn.commit()
