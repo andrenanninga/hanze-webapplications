3a5dccdd1e6f555342fe0718460548f0cf8f3e4a
Andre Nanninga <ag.nanninga@gmail.com>
2015-10-10 22:40:43 +0200

    first graph

--- opgaven/week3/opdracht2/src/app/measurements/views.py
+++ opgaven/week3/opdracht2/src/app/measurements/views.py
@@ -1,46 +1,77 @@
-from flask import Flask, Blueprint, Response
+from flask import Flask, Blueprint, Response, request, render_template
 from flask.ext.login import login_required, current_user
 from app import app, db
-from app.households.models import Device
+from app.households.models import HouseholdDevice, Device
 from operator import attrgetter
 import json
 
 mod = Blueprint('measurements', __name__, url_prefix='/measurements')
 
+@mod.route('/')
+@login_required
+def index():
+    householdDevices = current_user.households[0].householdDevices
+
+    for householdDevice in householdDevices:
+        householdDevice.name = householdDevice.device.name
+
+    return render_template('measurements/index.html', devices=householdDevices)
+
 @mod.route('/average-per-type')
 @login_required
 def average_per_type():
+    device_id = request.args.get('device')
+    area = request.args.get('area') or 'local'
+
+    if device_id is None:
+        return 'device parameter must be set', 400
+
+    householdDevice = HouseholdDevice.query.get(device_id)
+
     # Get the definition of a device
-    device = Device.query.filter_by(name='windmolen op dak').first()
+    device = Device.query.filter_by(id=householdDevice.device.id).first()
 
     # Get the id of every device installed in a household
     ids = map(attrgetter('id'), device.householdDevices)
     # Convert list to string
     ids = ','.join(str(id) for id in ids)
 
+    average = []
+    single = []
+
     # Get the average measurement for each hour
     query = """
-        SELECT AVG(waarde) AS average, tijd AS time
+        SELECT app_hh, AVG(waarde) AS average, tijd AS time
         FROM meting 
         WHERE app_hh IN ({ids})
         GROUP BY tijd;
-    """.format(ids=ids)
+    """
 
-    results = db.engine.execute(query)
-    measurements = []
-
-    # Map the result to dict measurements
+    # Get the averages of the all devices
+    results = db.engine.execute(query.format(ids=ids))
     for measurement in results:
-        measurements.append({
+        average.append({
             'time': str(measurement.time),
-            'average': str(measurement.average)
+            'average': float(measurement.average)
+        })
+
+    # Get the average of the housholddevice
+    results = db.engine.execute(query.format(ids=householdDevice.id))
+    for measurement in results:
+        single.append({
+            'time': str(measurement.time),
+            'average': float(measurement.average)
         })
 
     response = {
-        'name': device.name,
-        'max': device.max,
-        'measurements': measurements
+        'name': householdDevice.device.name,
+        'max': householdDevice.device.max,
+        'measurements': {
+            'single': single,
+            'average': average
+        }
     }
+
 
     # Return json with measurements
     return Response(json.dumps(response), status=200, mimetype='application/json')
---
+++ opgaven/week3/opdracht2/src/app/static/js/measurements.js
@@ -0,0 +1,52 @@
+'use strict';
+
+$(document).ready(function() {
+
+	$('select').on('change', function() {
+		loadMeasurements($('select[name=devices]').val(), $('select[name=area]').val());
+	});
+
+	loadMeasurements($('select[name=devices]').val(), $('select[name=area]').val());
+});
+
+function loadMeasurements(device, area) {
+	var url = '/measurements/average-per-type';
+	url += '?' + $.param({ device: device, area: area });
+
+	$.get(url, function(data) {
+		var times = _.map(_.pluck(data.measurements.average, 'time'), function(time) {
+			var date = new Date();
+			date.setHours(time.split(':')[0]);
+			date.setMinutes(time.split(':')[1]);
+			date.setSeconds(time.split(':')[2]);
+
+			return date;
+		});
+
+		var chart = c3.generate({
+			padding: {
+				bottom: 10
+			},
+			data: {
+				x: 'tijd',
+				columns: [
+					['tijd'].concat(times),
+					['mijn apparaat'].concat(_.pluck(data.measurements.single, 'average')),
+					['provinciaal gemiddelde'].concat(_.pluck(data.measurements.average, 'average'))
+				]
+			},
+			axis: {
+				x: {
+					label: 'meet tijd',
+					type: 'timeseries',
+					tick: {
+						format: '%H:%M:%S'
+					}
+				},
+				y: {
+					label: 'meting'
+				}
+			}
+		})
+	});
+}
